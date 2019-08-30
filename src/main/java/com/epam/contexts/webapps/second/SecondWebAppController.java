267cb2cca2749bdc6228682df04dbe098c13d890
dmitrii_berezin@epam.com <Aspirine666>
2019-08-30 18:00:35 +0300

    Added shared root context bean

--- src/main/java/com/epam/contexts/webapps/second/SecondWebAppController.java
+++ src/main/java/com/epam/contexts/webapps/second/SecondWebAppController.java
@@ -1,5 +1,6 @@
 package com.epam.contexts.webapps.second;
 
+import com.epam.contexts.service.MessagingService;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -7,8 +8,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class SecondWebAppController implements Controller {
+
+    private MessagingService messagingService;
+
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return new ModelAndView("not-welcome", "message", "HI ALL!");
+        return new ModelAndView("not-welcome", "message", messagingService.getMessage());
+    }
+
+    public MessagingService getMessagingService() {
+        return messagingService;
+    }
+
+    public void setMessagingService(MessagingService messagingService) {
+        this.messagingService = messagingService;
     }
 }
--- src/main/java/com/epam/contexts/webapps/third/ThirdWebAppController.java
+++ src/main/java/com/epam/contexts/webapps/third/ThirdWebAppController.java
@@ -1,5 +1,6 @@
 package com.epam.contexts.webapps.third;
 
+import com.epam.contexts.service.MessagingService;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -7,8 +8,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ThirdWebAppController implements Controller {
+
+    private MessagingService messagingService;
+
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return new ModelAndView("welcome", "message", "HI almost ALL!");
+        return new ModelAndView("welcome", "message", messagingService.getMessage());
+    }
+
+    public MessagingService getMessagingService() {
+        return messagingService;
+    }
+
+    public void setMessagingService(MessagingService messagingService) {
+        this.messagingService = messagingService;
     }
 }
