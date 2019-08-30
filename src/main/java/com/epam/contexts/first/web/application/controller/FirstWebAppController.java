e938b69b687fc76efbae18c8689a5c4c628c98e3
dmitrii_berezin@epam.com <Aspirine666>
2019-08-30 16:23:58 +0300

    WIP: Rid of java config

--- src/main/java/com/epam/contexts/first/web/application/controller/FirstWebAppController.java
+++ src/main/java/com/epam/contexts/first/web/application/controller/FirstWebAppController.java
@@ -1,42 +1,55 @@
 package com.epam.contexts.first.web.application.controller;
 
 import com.epam.contexts.service.MessagingService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.context.ContextLoader;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@Controller
-public class FirstWebAppController {
+public class FirstWebAppController implements Controller {
 
     private static Logger logger = Logger.getLogger(FirstWebAppController.class.getName());
 
-    @Autowired
     private MessagingService messagingService;
 
-    @Autowired
     private WebApplicationContext webApplicationContext;
 
     private void processContext() {
         WebApplicationContext rootContext = ContextLoader.getCurrentWebApplicationContext();
 
-        logger.log(Level.INFO,"Here root context: " + rootContext);
-        logger.log(Level.INFO,"And root context Beans: " + Arrays.asList(rootContext.getBeanDefinitionNames()));
+        logger.log(Level.INFO,"Here root context: {0}", rootContext);
+        logger.log(Level.INFO,"And root context Beans: {0}", Arrays.asList(rootContext.getBeanDefinitionNames()));
 
-        logger.log(Level.INFO, "Then, application context: " + webApplicationContext);
-        logger.log(Level.INFO,"And application context Beans: " + Arrays.asList(webApplicationContext.getBeanDefinitionNames()));
+        logger.log(Level.INFO, "Then, application context: {0}", webApplicationContext);
+        logger.log(Level.INFO,"And application context Beans: {0}", Arrays.asList(webApplicationContext.getBeanDefinitionNames()));
     }
 
-    @GetMapping(path = "/welcome")
-    public ModelAndView example() {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
         processContext();
         String message = "<br><div style='text-align:center;'>" + "<h3>Normal " + messagingService.getMessage() + "</h3></div>";
         return new ModelAndView("welcome", "message", message);
+    }
+
+    public MessagingService getMessagingService() {
+        return messagingService;
+    }
+
+    public void setMessagingService(MessagingService messagingService) {
+        this.messagingService = messagingService;
+    }
+
+    public WebApplicationContext getWebApplicationContext() {
+        return webApplicationContext;
+    }
+
+    public void setWebApplicationContext(WebApplicationContext webApplicationContext) {
+        this.webApplicationContext = webApplicationContext;
     }
 }
--- src/main/java/com/epam/contexts/service/MessagingService.java
+++ src/main/java/com/epam/contexts/service/MessagingService.java
@@ -1,17 +1,16 @@
 package com.epam.contexts.service;
 
 import com.epam.contexts.model.MessageToPass;
-import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-
-@Service
 public class MessagingService {
 
-    @Resource
     private MessageToPass message;
 
     public String getMessage() {
         return message.getMessage();
+    }
+
+    public void setMessage(MessageToPass message) {
+        this.message = message;
     }
 }
